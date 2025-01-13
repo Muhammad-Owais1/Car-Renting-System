3d8c437ad74bcab2ec141a080e7869a9ba731ffe
MuhammadOwais <141480957+Muhammad-Owais1@users.noreply.github.com>
2025-01-13 16:01:21 +0500

    fix

--- server/index.js
+++ server/index.js
@@ -11,12 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.options("*", cors());
-app.use(
-  cors({
-    origin: "https://car-renting-system.vercel.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
