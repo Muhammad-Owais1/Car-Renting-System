37f92b1acd347761f923f609500ea2e0404018cf
MuhammadOwais <141480957+Muhammad-Owais1@users.noreply.github.com>
2025-01-13 20:49:36 +0500

    fix

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.use(
   cors({
     origin: "https://car-renting-system.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 app.options("*", cors());
