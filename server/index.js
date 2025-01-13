90450357bb57e117d45bd7c0070776f5b89d38aa
MuhammadOwais <141480957+Muhammad-Owais1@users.noreply.github.com>
2025-01-13 23:49:58 +0500

    fix

--- server/index.js
+++ server/index.js
@@ -12,14 +12,15 @@ const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "https://car-renting-system.vercel.app",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
-app.options("*", cors());
+// app.use(
+//   cors({
+//     origin: "https://car-renting-system.vercel.app",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
+// app.options("*", cors());
+app.use(cors());
 app.use(cookieParser());
 
 mongoose.connect(process.env.DB_URL);
