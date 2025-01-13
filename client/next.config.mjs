cac51fcef7b998ea021628cff2975e2c5dd59839
MuhammadOwais <141480957+Muhammad-Owais1@users.noreply.github.com>
2025-01-13 19:52:41 +0500

    fix

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Match any route starting with /api
+        destination: "https://car-renting-system-api.vercel.app/api/:path*", // Proxy to your backend
+      },
+    ];
+  },
+};
 
 export default nextConfig;
